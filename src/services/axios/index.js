e1bf02217f87bd83f28fe99b26adf78aed82c49e
wesley-oliveira-santos-dev <wesley.oliveira.dev@gmail.com>
2022-03-02 19:50:46 -0300

    add router aws

--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -3,11 +3,11 @@ import axios from "axios"
 // https://codedcreatures.com/2020/02/17/creating-and-using-axios-instances-in-react/
 
 const unAuthenticatedAxiosInstance = axios.create({
-   baseURL: 'https://d2hlik0ak2.execute-api.us-east-2.amazonaws.com'
+   baseURL: 'https://kj4fdqr31d.execute-api.sa-east-1.amazonaws.com/assembleia-sinpeem'
 });
 
 const authenticatedInstance = axios.create({
-   baseURL: 'https://d2hlik0ak2.execute-api.us-east-2.amazonaws.com'
+   baseURL: 'https://kj4fdqr31d.execute-api.sa-east-1.amazonaws.com/assembleia-sinpeem'
 });
 
 authenticatedInstance.defaults.headers.common['Authorization'] = localStorage.getItem('authenticationToken');
